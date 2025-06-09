a7822d645cbc3ca0dcb0d767804915fbd11de71c
Erik Salas <eriksalas@MacBook-Air-de-Erik-2.local>
2025-06-09 15:08:16 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'antiasalto-gps',
+  webDir: 'www'
+};
+
+export default config;
